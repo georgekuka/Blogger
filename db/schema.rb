6aba87fe4af6f0537e7e3ee25282a1cd2668d759
George Kuka <gk3biz@gmail.com>
2014-08-06 18:22:29 -0700

    added image attachment

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806213822) do
+ActiveRecord::Schema.define(version: 20140807003632) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "images_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: true do |t|
